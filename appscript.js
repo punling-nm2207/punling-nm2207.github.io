5605b0cad5402360a335af416caeb07bece4bfd0
punling-nm2207 <125462818+punling-nm2207@users.noreply.github.com>
2023-03-18 22:41:25 +0800

    NM2207 Project
    Week 9 submission

---
+++ appscript.js
@@ -0,0 +1,19 @@
+//adapted from nm2207 content and https://www.w3schools.com/howto/howto_js_tabs.asp-
+
+function openBerry(evt, tabName) {
+    var i, tabcontent, tablinks;
+  
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
